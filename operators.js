da1893057f056b7e5527a353437e0a7e3a236fbb
SiddheshDaphane <siddhesh.daphane@gmail.com>
2025-05-11 14:31:45 -0500

    Wrote a code about a prompt which opens a pop up everytime when you refersh a page.

--- operators.js
+++ operators.js
@@ -40,3 +40,13 @@ let age = 20;
 result = age >= 18 ? "adult": "not adult";
 console.log(result)
 }
+
+{
+  let num = prompt("enter a number")
+
+  if (num % 3 === 0) {
+    console.log(num, "is a multiple of 3");
+  } else {
+    console.log(num, "is NOT a multiple of 3");
+  }
+}
