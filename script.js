477af7e7f0c3c6752183fdf49661471731742837
SiddheshDaphane <siddhesh.daphane@gmail.com>
2025-05-12 18:51:01 -0500

    sum of first n numbers using loop

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+// First loop. 
+
 for (let i = 1; i <=5; i++) {
   console.log("Siddhesh Daphane")
 }
 
 console.log("Loop has ended.")
+
+// Calculate sum of 1 to n.
+let sum = 0;
+for (let i = 1; i<=5; i++){
+  sum = sum + i;
+}
+console.log("sum is ",sum)
