dae1387bda8c2f1fe4439c8e3782819ecefc7703
SiddheshDaphane <siddhesh.daphane@gmail.com>
2025-05-12 19:48:23 -0500

    Even number question

--- script.js
+++ script.js
@@ -48,3 +48,12 @@ let student = {
 for(let key in student){
   console.log("key is = ",key, " and it's value = ", student[key]);
 } 
+
+// Even numbers
+for(let i=1; i<=100; i++){
+  if (i%2===0) {
+    console.log(i);
+  } else {
+    console.log("Number is odd");
+  }
+}
