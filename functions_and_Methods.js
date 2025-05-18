a3f4bba0895b15c5feb8633de6a3461203696081
SiddheshDaphane <siddhesh.daphane@gmail.com>
2025-05-18 08:06:42 -0500

    Writing function conecpets here.

---
+++ functions_and_Methods.js
@@ -0,0 +1,12 @@
+function myFunction() {
+  console.log("Siddhesh Daphane")
+  console.log("SOftware Engineer")
+}
+
+myFunction();
+
+function myFunction1(msg) {
+  console.log(msg)
+}
+
+myFunction1("One piece")
