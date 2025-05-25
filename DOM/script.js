c2211eca86098896de38d42af7f0df04f518cdcd
SiddheshDaphane <siddhesh.daphane@gmail.com>
2025-05-25 17:43:30 -0500

    getting element by ID in js

--- DOM/script.js
+++ DOM/script.js
@@ -3,3 +3,8 @@ console.dir(document)
 console.log(document)
 console.log(document.body)
 console.log(document.head)
+
+// getting element with ID in JS. 
+let heading = document.getElementById("heading");
+console.dir(heading)
+
