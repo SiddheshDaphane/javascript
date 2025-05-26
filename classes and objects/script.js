3b8c908f8345da14ae9108ba01421cb9308cc9c7
SiddheshDaphane <siddhesh.daphane@gmail.com>
2025-05-26 14:37:49 -0500

    using prototype to get functions of one object in another

--- classes and objects/script.js
+++ classes and objects/script.js
@@ -5,3 +5,17 @@ const student = {
     console.log("marks = ", this.marks) // "this" keyword is like "self" in python. So "this.marks" is "student.marks"
   }
 }
+
+// Prototype
+
+const employee = {
+  calcTax() {
+    console.log("Tax rate is 10%")
+  }
+}
+
+const siddhesh = {
+  salary : 70000
+}
+
+siddhesh.__proto__ = employee; // employee object functions can be used in "siddhesh" object because of this line of code. 
