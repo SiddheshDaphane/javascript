ac261a0d2c05ecb9ed008e8b329ea98b74976aff
SiddheshDaphane <siddhesh.daphane@gmail.com>
2025-05-26 09:59:11 -0500

    accessing event object of a button

--- Events/script.js
+++ Events/script.js
@@ -1,3 +1,5 @@
+// Accessing elements and their events
+
 let btn = document.querySelector("#btn")
 btn.onclick = () => {
   console.log("button was clicked!");
@@ -9,4 +11,13 @@ btn.onclick = () => {
 let box = document.querySelector(".box")
 box.onmouseover = () => {
   console.log("You are inside the div!")
+}
+
+// Event Object. 
+let btnEvent = document.querySelector("#btn")
+btnEvent.onclick = (e) => {
+  console.log(e);
+  console.log(e.type);
+  console.log(e.target);
+  console.log(e.clientX, e.clientY);
 }
