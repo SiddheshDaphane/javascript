afcd0e46a7f190c18dc280db4102fe73fba35b5c
SiddheshDaphane <siddhesh.daphane@gmail.com>
2025-05-26 15:09:53 -0500

    accessing constructors attrbute or parameters in different methods of same object using this keyword

--- classes and objects/script.js
+++ classes and objects/script.js
@@ -72,3 +72,30 @@ class Car {
 
 let lambo = new Car();
 let ferrari = new Car();
+
+
+class Car1 {
+  constructor(brand, milega) {
+    console.log("Invoking constructor while creating a new object")
+    this.brand = brand, 
+    this.milega = milega
+  }
+
+  start() {
+    console.log("start")
+    console.log(this.brand)
+  }
+
+  stop() {
+    console.log("stop")
+    console.log(this.milega)
+  }
+
+  setBrand(brand) {
+    this.brand = brand;
+    console.log(brand);
+  }
+}
+
+let maruti = new Car1("4 stroke", 24);
+let honda = new Car1("vtech", 19);
