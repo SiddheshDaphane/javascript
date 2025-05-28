9c439331ff44f85dd992cefc681bc0799e03dd95
SiddheshDaphane <siddhesh.daphane@gmail.com>
2025-05-28 14:51:01 -0500

    Callback hell concept

---
+++ callbacks, promises & async await/script.js
@@ -0,0 +1,28 @@
+// callbacks using setTimeout
+
+function getData(dataId) {
+  setTimeout(() => {
+    console.log("data ", dataId);
+  },2000)
+}
+
+getData(1)
+getData(2)
+getData(3)
+
+// callback hell (Need to look at this again)
+
+function getDataHell(dataId, getNextData) {
+  setTimeout(() => {
+    console.log("data", dataId);
+    if (getNextData){
+    getNextData();
+    }
+  },2000)
+}
+
+getDataHell(1, () => {
+  getDataHell(2, () => {
+    getDataHell(3)
+  })
+}) 
